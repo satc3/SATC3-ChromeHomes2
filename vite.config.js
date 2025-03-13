57dbf7367151c8a8e2cd9bfbc13e3a073b9a3563
satc3 <satceee@gmail.com>
2025-03-13 17:12:52 +0900

    manifest.jsonとvite.config.jsを更新し、ポップアップとサービスワーカーのパスを修正しました。また、content_scriptsを追加し、Reactプラグインをvite.config.jsに統合しました。

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import webExtension from 'vite-plugin-web-extension';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -7,8 +8,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [
+    react(),
     webExtension({
       manifest: './manifest.json',
+      webExtConfig: {
+        startUrl: ['https://example.com'],
+      },
     }),
   ],
   resolve: {
